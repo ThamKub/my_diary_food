af1c2828a063f372d5c5a9bc2c3675e9a10c6cce
ThamKub <s6452410012@sau.ac.th>
2023-10-11 16:46:16 +0700

    Code DiaryFood V.2

---
+++ lib/views/modify_diaryfood_ui.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ModifyDiaryfoodUI extends StatefulWidget {
+  const ModifyDiaryfoodUI({super.key});
+
+  @override
+  State<ModifyDiaryfoodUI> createState() => _ModifyDiaryfoodUIState();
+}
+
+class _ModifyDiaryfoodUIState extends State<ModifyDiaryfoodUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
